53984e1759ac3eebee89d21563e9f436a2205309
Anton Zaharia <zaharia_anton@yahoo.com>
2021-12-21 11:57:05 +0000

    check for ethereum

--- tip.js
+++ tip.js
@@ -1,5 +1,9 @@
 const form = document.querySelector('form')
 
+if (window.ethereum) {
+    form.classList.add('has-eth')
+}
+
 form.addEventListener('submit', function (event) {
     event.preventDefault()
     console.log('submit!')
